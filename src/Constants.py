9d50af6a73ea461f9a8051e02503832a380b96ec
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2020-06-12 12:06:19 +0200

    Images data are saved as a pickle

--- src/Constants.py
+++ src/Constants.py
@@ -1,4 +1,7 @@
+import os
+
 DATASET = "anonymous_img_biotouch_2018_dataset"
 MINI_DATASET = "mini_anonymous_img_biotouch_2018_dataset"
 
 DATASET_DIRECTORY_PATH = "/universita/cache/dataset/"
+PICKLE_DATA_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__)).replace("src","")+"res\\"
--- src/DataManager.py
+++ src/DataManager.py
@@ -19,50 +19,56 @@ class DataManager:
 
     def load_images_data(self, update_data = False):
         if not update_data and DataManager._check_saved_pickles(self.dataset_name):
-            print("VANNO LETTI I PICKLE? Direi di sì....quando famo sto dataset poi lo mettemo a pickle")
+            self.read_pickle_data()
         else:
             self.generate_data()
-            # self.save_data()
-
+            self.save_pickle_data()
         # self.dataset.getData()['BLOCK LETTERS'][0].getImages()[0][0] è IL np.array che rappresenta un'immagine di BLOCK_LETTERS
         # della prima sessione del primo utente
         # print(self.dataset.getData()['BLOCK LETTERS'][0].getImages()[0][0],
         #       type(self.dataset.getData()['BLOCK LETTERS'][0].getImages()[0][0]))
         #
         # print(self.dataset.getData()['BLOCK LETTERS'][0].getImages()[0][0].shape)
-    def save_data(self):
-        dataset_pickle_path = ""
-        pickle.dump(self.dataset, open(dataset_pickle_path, "wb"))
 
-    def generate_data(self):
-        self.dataset = PyImgDataset()
-        # print(os.listdir("./test/"+self.dataset_name))
-        dataset_path = Constants.DATASET_DIRECTORY_PATH+self.dataset_name+"/"
-        for user_folder in os.listdir(dataset_path):
-            # print(user_folder)
-            user_folder_path = dataset_path+user_folder+"/"
-            for session_number in os.listdir(user_folder_path):
-                # print("    "+session_number)
-                for writing_style in os.listdir(user_folder_path+session_number):
-                    # print(writing_style)
-                    currentSession = PyImgSession(user_folder, session_number) #I DATI DELLA SESSIONE
-                    # print(s)
-                    for img in os.listdir(user_folder_path+session_number+"/"+writing_style):
-                        img_path = user_folder_path+session_number+"/"+writing_style+"/"+img
-                        img_png= np.array(Image.open(img_path))
-                        currentSession.add_image([np.array(img_png), ("s"+session_number+"."+img).replace(".png", "")])
-                        # print(("s"+session_number+"."+img).replace(".png", ""))
-                    self.dataset.add_session(writing_style, currentSession)
 
+    def read_pickle_data(self):
+        dataset_pickle_path = Constants.PICKLE_DATA_DIRECTORY_PATH+self.dataset_name
+        pickle_in = open(dataset_pickle_path, "rb")
+        self.dataset = pickle.load(pickle_in)
+        # print(self.dataset.getData())
+        pickle_in.close()
+
+
+    def save_pickle_data(self):
+        dataset_pickle_path = Constants.PICKLE_DATA_DIRECTORY_PATH+self.dataset_name
+        pickle_out = open(dataset_pickle_path, "wb")
+        pickle.dump(self.dataset, pickle_out)
+        print("PICKLES FILE ARE SAVED!")
+        pickle_out.close()
 
     @staticmethod
     def _check_saved_pickles(dataset_name):
-        # for label in DATAFRAMES:
-        #     if not Utils.os.path.isfile(Utils.BUILD_DATAFRAME_PICKLE_PATH(dataset_name, label)):
-        #         return False
-        # return True
-        return False
+        return os.path.isfile(Constants.PICKLE_DATA_DIRECTORY_PATH+dataset_name)
+
+    def generate_data(self):
+        self.dataset = PyImgDataset()
+        dataset_path = Constants.DATASET_DIRECTORY_PATH+self.dataset_name+"/"
+        for user_folder in os.listdir(dataset_path):
+            user_folder_path = dataset_path+user_folder+"/"
+            for session_number in os.listdir(user_folder_path):
+                for writing_style in os.listdir(user_folder_path+session_number):
+                    currentSession = PyImgSession(user_folder, session_number) #I DATI DELLA SESSIONE
+                    for img in os.listdir(user_folder_path+session_number+"/"+writing_style):
+                        img_png= np.array(Image.open(user_folder_path+session_number+"/"+writing_style+"/"+img))
+                        img_text_components = img.split(".")
+                        img_text_components.remove('png')
+                        # img_text_components IS SOMETHING LIKE: ['s0', 'u2', 'ITALIC', '28'] so [ session_code, class_code, writing_style, token_number_of_the_session]
+                        img_text_components.insert(0,"s"+session_number)
+                        currentSession.add_image([np.array(img_png), img_text_components])
+                    self.dataset.add_session(writing_style, currentSession)
 
 if __name__=='__main__':
-    print((os.path.dirname(os.path.abspath(__file__))).replace("src","")+"res\\")
-    # d = DataManager(Constants.MINI_DATASET)
+    # for user_folder in os.listdir(Constants.DATASET_DIRECTORY_PATH):
+    #     print(user_folder)
+    # print(Constants.DATASET_DIRECTORY_PATH)
+    d = DataManager(Constants.MINI_DATASET)
